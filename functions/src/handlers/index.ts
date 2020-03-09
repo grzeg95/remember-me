9d954d55ea21b26390768526e9257dfc0720bbc7
GrzegorzEleryk <grzegorz.1995@o2.pl>
2020-03-09 19:26:56 +0100

    feat: setTodayOrder

--- functions/src/handlers/index.ts
+++ functions/src/handlers/index.ts
@@ -1,3 +1,4 @@
 export {handler as setProgressHandler} from './setProgress';
 export {handler as saveTaskHandler} from './saveTask';
 export {handler as deleteTaskHandler} from './deleteTask';
+export {handler as setTodayOrderHandler} from './setTodayOrder';
