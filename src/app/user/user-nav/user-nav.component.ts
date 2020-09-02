4821985ec2b2767b7743611841d421abdc457e0a
GrzegorzEleryk <grzegorz.1995@o2.pl>
2020-09-02 19:48:27 +0200

    feat: removed user nav icons

--- src/app/user/user-nav/user-nav.component.ts
+++ src/app/user/user-nav/user-nav.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {faAlignLeft, faListUl, faPencilAlt, faSort} from '@fortawesome/free-solid-svg-icons';
 import {RouterDict} from '../../app.constants';
 
 @Component({
@@ -12,22 +11,18 @@ export class UserNavComponent {
 
   routes = [
     {
-      icon: faAlignLeft,
       label: 'Today',
       href: '/' + RouterDict['user'] + '/' + RouterDict['today']
     },
     {
-      icon: faListUl,
       label: 'Tasks',
       href: '/' + RouterDict['user'] + '/' + RouterDict['tasks']
     },
     {
-      icon: faPencilAlt,
       label: 'Editor',
       href: '/' + RouterDict['user'] + '/' + RouterDict['task']
     },
     {
-      icon: faSort,
       label: 'Order',
       href: '/' + RouterDict['user'] + '/' + RouterDict['times-of-day-order']
     }
