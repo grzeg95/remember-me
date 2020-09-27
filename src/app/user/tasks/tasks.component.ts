7e466e237512549551860256ab54de3be2accb26
GrzegorzEleryk <grzegorz.1995@o2.pl>
2020-09-27 11:17:44 +0200

    feat: use material

--- src/app/user/tasks/tasks.component.ts
+++ src/app/user/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faEdit} from '@fortawesome/free-regular-svg-icons';
-import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import '../../../../global.prototype';
 import {AppService} from '../../app-service';
 import {RouterDict} from '../../app.constants';
@@ -12,7 +12,7 @@ import {UserService} from '../user.service';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit, OnDestroy {
+export class TasksComponent implements OnInit {
 
   get timesOfDayOrderFirstLoading$(): Observable<boolean> {
     return this.userService.timesOfDayOrderFirstLoading$;
@@ -30,10 +30,13 @@ export class TasksComponent implements OnInit, OnDestroy {
     return this.appService.isConnected$;
   }
 
+  get tasks$(): BehaviorSubject<TasksListItem[]> {
+    return this.userService.tasks$;
+  }
+
   RouterDict = RouterDict;
   faEdit = faEdit;
-  tasksView$: BehaviorSubject<TasksListItem[]> = new BehaviorSubject([]);
-  tasksViewSub: Subscription = new Subscription();
+  displayedColumns: string[] = ['description', 'daysOfTheWeek', 'timesOfDays', 'edit'];
 
   constructor(private userService: UserService,
               private appService: AppService) {}
@@ -41,40 +44,12 @@ export class TasksComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userService.runTimesOfDayOrder();
     this.userService.runTasks();
-
-    this.tasksViewSub.add(this.userService.tasks$.subscribe(() => this.updateTasksViews()));
-    this.tasksViewSub.add(this.userService.timesOfDayOrder$.subscribe(() => this.updateTasksViews()));
   }
 
-  ngOnDestroy(): void {
-    this.tasksViewSub.unsubscribe();
-  }
-
-  updateTasksViews(): void {
-
-    const tasks = this.userService.tasks$.getValue();
-    const timesOfDayOrder = this.userService.timesOfDayOrder$.getValue().map((val) => val.id);
-
-    const timesOfDayOrderSet = timesOfDayOrder.toSet();
-    this.tasksView$.next(tasks.map((taskListItem) => {
-
-      const timesOfDayOrderSetIntersectionTaskListItemTimesOfDay = timesOfDayOrderSet.intersection((taskListItem.timesOfDay as string[]).toSet());
-      const timesOfDayOrderTmp = [];
-
-      for (const x of timesOfDayOrder) {
-        if (timesOfDayOrderSetIntersectionTaskListItemTimesOfDay.has(x)) {
-          timesOfDayOrderTmp.push(x);
-          timesOfDayOrderSetIntersectionTaskListItemTimesOfDay.delete(x);
-        }
-      }
-
-      taskListItem.timesOfDay = timesOfDayOrderTmp;
-      return taskListItem;
-    }));
-  }
-
-  getTimesOfDay(timesOfDay: string[]): string[] {
-    return timesOfDay.map((timeOfDay) => timeOfDay.decodeFirebaseSpecialCharacters());
+  getTimesOfDay(timesOfDayOrder: TimeOfDay[], taskTimesOfDay: string[]): string[] {
+    const taskTimesOfDaySet = taskTimesOfDay.toSet();
+    return timesOfDayOrder.filter((timeOfDayOrderItem) => taskTimesOfDaySet.has(timeOfDayOrderItem.id))
+      .map((timeOfDayOrderItem) => timeOfDayOrderItem.id.decodeFirebaseSpecialCharacters());
   }
 
 }
