fd9aaa48eafee2f547b645341d25e040074d81e5
GrzegorzEleryk <grzegorz.1995@o2.pl>
2021-03-30 22:49:37 +0200

    feat(functions): start using PyCharm 😁

--- functions/minify.py
+++ functions/minify.py
@@ -1,25 +1,43 @@
 import multiprocessing as mp
 import os
 import threading
+from multiprocessing.queues import JoinableQueue
 import cursor
 
-def printInLine(text):
-  print(str(text), end='\r')
 
-def map(x, in_min, in_max, out_min, out_max):
+def print_in_line(maybe_text: any) -> None:
+  print(str(maybe_text), end='\r')
+
+
+def print_progress(maybe_text: any) -> None:
+  if type(maybe_text) is str:
+    print_in_line('uglifyjs functions: {}   '.format(maybe_text))
+  elif type(maybe_text) is int or type(maybe_text) is float:
+    print_in_line('uglifyjs functions: {:.2f}%   '.format(maybe_text))
+
+
+def map(
+  x: float or int,
+  in_min: float or int,
+  in_max: float or int,
+  out_min: float or int,
+  out_max: float or int
+) -> float:
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def worker(queue, size):
-  for path in iter(queue.get, None):
-    printInLine('uglifyjs functions: {:.2f}%   '.format(map(queue.qsize(), size, 0, 0, 100)))
+
+def worker(q: JoinableQueue, q_init_size: int) -> None:
+  for path in iter(q.get, None):
+    print_progress(map(q.qsize(), q_init_size, 0, 0, 100))
     os.system('uglifyjs {0} -c -o {0}'.format(path))
-    printInLine('uglifyjs functions: {:.2f}%   '.format(map(queue.qsize(), size, 0, 0, 100)))
-    queue.task_done()
+    print_progress(map(q.qsize(), q_init_size, 0, 0, 100))
+    q.task_done()
+
 
 if __name__ == '__main__':
 
   cursor.hide()
-  printInLine('uglifyjs functions: 0%\r')
+  print_progress(0)
 
   queue = mp.JoinableQueue()
 
@@ -29,9 +47,12 @@ if __name__ == '__main__':
         queue.put(os.path.join(root, file))
 
   worker_count = mp.cpu_count()
-  threads = [threading.Thread(target=worker, args=[queue, queue.qsize()], daemon=True) for _ in range(worker_count)]
+  threads = [
+    threading.Thread(target=worker, args=[queue, queue.qsize()], daemon=True)
+    for _ in range(worker_count)
+  ]
 
-  for t in threads: # start workers
+  for t in threads:  # start workers
     t.start()
 
   for _ in threads:  # signal workers to quit
@@ -40,6 +61,4 @@ if __name__ == '__main__':
   for t in threads:  # wait until workers exit
     t.join()
 
-  # additional spaces to clear line
-  #          ('uglifyjs functions: 99.99%\n')
-  printInLine('uglifyjs functions: done  \n')
+  print_progress('done')
