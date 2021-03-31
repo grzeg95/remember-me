78fd3f72a717b94bf7bdd59f7159c64bb335d6c1
GrzegorzEleryk <grzegorz.1995@o2.pl>
2021-03-31 21:25:56 +0200

    fix(script): use union for types

--- functions/minify.py
+++ functions/minify.py
@@ -3,34 +3,35 @@ import os
 import threading
 from multiprocessing.queues import JoinableQueue
 import cursor
+from typing import Union
 
 
-def print_in_line(maybe_text: any) -> None:
+def print_in_line(maybe_text: Union[str, int, float]) -> None:
   print(str(maybe_text), end='\r')
 
 
-def print_progress(maybe_text: any) -> None:
+def print_progress(maybe_text: Union[str, int, float]) -> None:
   if type(maybe_text) is str:
     print_in_line('uglifyjs functions: {}   '.format(maybe_text))
   elif type(maybe_text) is int or type(maybe_text) is float:
     print_in_line('uglifyjs functions: {:.2f}%   '.format(maybe_text))
 
 
-def map(
-  x: float or int,
-  in_min: float or int,
-  in_max: float or int,
-  out_min: float or int,
-  out_max: float or int
+def re_maps_number_from_one_range_to_another(
+  x: Union[int, float],
+  in_min: Union[int, float],
+  in_max: Union[int, float],
+  out_min: Union[int, float],
+  out_max: Union[int, float]
 ) -> float:
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 def worker(q: JoinableQueue, q_init_size: int) -> None:
   for path in iter(q.get, None):
-    print_progress(map(q.qsize(), q_init_size, 0, 0, 100))
+    print_progress(re_maps_number_from_one_range_to_another(q.qsize(), q_init_size, 0, 0, 100))
     os.system('uglifyjs {0} -c -o {0}'.format(path))
-    print_progress(map(q.qsize(), q_init_size, 0, 0, 100))
+    print_progress(re_maps_number_from_one_range_to_another(q.qsize(), q_init_size, 0, 0, 100))
     q.task_done()
 
 
