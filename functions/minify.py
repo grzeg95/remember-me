23d337908b085e6a6091d4f9d5b34d3a1a89eb9f
GrzegorzEleryk <grzegorz.1995@o2.pl>
2021-04-01 19:07:57 +0200

    fix(script): observe progress

--- functions/minify.py
+++ functions/minify.py
@@ -4,17 +4,32 @@ import threading
 from multiprocessing.queues import JoinableQueue
 import cursor
 from typing import Union
+from observable import Observable
 
 
-def print_in_line(maybe_text: Union[str, int, float]) -> None:
-  print(str(maybe_text), end='\r')
+obs = Observable()
+lock = threading.Lock()
 
 
-def print_progress(maybe_text: Union[str, int, float]) -> None:
-  if type(maybe_text) is str:
-    print_in_line('uglifyjs functions: {}   '.format(maybe_text))
-  elif type(maybe_text) is int or type(maybe_text) is float:
-    print_in_line('uglifyjs functions: {:.2f}%   '.format(maybe_text))
+@obs.on('progress')
+def print_progress(message: str):
+  print(message, end='\r')
+
+
+def update_progress(maybe_text: Union[str, int, float], progress_to_update) -> None:
+
+  with lock:
+
+    if type(maybe_text) is str:
+      progress_to_update[0] = maybe_text
+      obs.trigger('progress', 'uglifyjs functions: {}   '.format(progress_to_update[0]))
+
+    elif type(progress_to_update[0]) is not str and type(maybe_text) is int or type(maybe_text) is float:
+
+      if maybe_text > progress_to_update[0]:
+
+        progress_to_update[0] = maybe_text
+        obs.trigger('progress', 'uglifyjs functions: {:.2f}%   '.format(maybe_text))
 
 
 def re_maps_number_from_one_range_to_another(
@@ -27,18 +42,16 @@ def re_maps_number_from_one_range_to_another(
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-def worker(q: JoinableQueue, q_init_size: int) -> None:
+def worker(q: JoinableQueue, progress_to_work_with) -> None:
   for path in iter(q.get, None):
-    print_progress(re_maps_number_from_one_range_to_another(q.qsize(), q_init_size, 0, 0, 100))
     os.system('uglifyjs {0} -c -o {0}'.format(path))
-    print_progress(re_maps_number_from_one_range_to_another(q.qsize(), q_init_size, 0, 0, 100))
+    update_progress(re_maps_number_from_one_range_to_another(q.qsize(), progress_to_work_with[1], 0, 0, 100), progress_to_work_with)
     q.task_done()
 
 
 if __name__ == '__main__':
 
   cursor.hide()
-  print_progress(0)
 
   queue = mp.JoinableQueue()
 
@@ -47,9 +60,12 @@ if __name__ == '__main__':
       if file.endswith(".js"):
         queue.put(os.path.join(root, file))
 
+  progress = [0, queue.qsize()]
+  update_progress(0, progress)
+
   worker_count = mp.cpu_count()
   threads = [
-    threading.Thread(target=worker, args=[queue, queue.qsize()], daemon=True)
+    threading.Thread(target=worker, args=[queue, progress], daemon=True)
     for _ in range(worker_count)
   ]
 
@@ -62,4 +78,4 @@ if __name__ == '__main__':
   for t in threads:  # wait until workers exit
     t.join()
 
-  print_progress('done')
+  update_progress('done', progress)
