7d6a2f7cc061820935f7bd927a71bd53827a2e88
grzeg <grzegorz.eleryk@gmail.com>
2021-04-10 08:50:07 +0200

    fix(functions): use guestComponentTrace

--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -1,14 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {faEdit, faListAlt} from '@fortawesome/free-regular-svg-icons';
 import {faChartArea, faListOl, faProjectDiagram, faSort} from '@fortawesome/free-solid-svg-icons';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons';
+import {AngularFirePerformance} from '@angular/fire/performance';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-guest',
   templateUrl: './guest.component.html',
   styleUrls: ['./guest.component.scss']
 })
-export class GuestComponent {
+export class GuestComponent implements OnInit, OnDestroy {
+
+  private guestComponentTrace: Promise<firebase.performance.Trace | void>;
+
+  constructor(private perf: AngularFirePerformance) {
+  }
 
   faEdit = faEdit;
   faListAlt = faListAlt;
@@ -18,4 +25,11 @@ export class GuestComponent {
   faGoogle = faGoogle;
   faChartArea = faChartArea;
 
+  ngOnInit(): void {
+    this.guestComponentTrace = this.perf.trace('GuestComponent').then((e) => e && e.start());
+  }
+
+  ngOnDestroy(): void {
+    this.guestComponentTrace.then((e) => e && e.stop());
+  }
 }
