3fe04ea876f0b45753c060115a14d27d114c40e1
GrzegorzEleryk <grzegorz.eleryk@gmail.com>
2022-02-07 22:33:33 +0100

    security: chore lole update user indexedDB on login

--- src/app/guest/guest-about-security/guest-about-security.component.ts
+++ src/app/guest/guest-about-security/guest-about-security.component.ts
@@ -3,7 +3,8 @@ import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-guest-about-security',
-  templateUrl: './guest-about-security.component.html'
+  templateUrl: './guest-about-security.component.html',
+  styleUrls: ['./guest-about-security.component.scss']
 })
 export class GuestAboutSecurityComponent {
 
