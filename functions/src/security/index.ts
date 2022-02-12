c9a58674bbdf913248856eb65bd28888a8dffd26
GrzegorzEleryk <grzegorz.eleryk@gmail.com>
2022-02-12 10:35:31 +0100

    change timeoutSeconds for security functions

--- functions/src/security/index.ts
+++ functions/src/security/index.ts
@@ -2,13 +2,13 @@ import {runWith} from 'firebase-functions';
 import {regionId} from '../config';
 
 export const getSymmetricKey = runWith({
-  timeoutSeconds: 30,
+  timeoutSeconds: 60,
   memory: '512MB',
   maxInstances: 2
 }).region(regionId).https.onCall(require('./get-symmetric-key').handler);
 
 export const userOnCreate = runWith({
-  timeoutSeconds: 30,
+  timeoutSeconds: 60,
   memory: '512MB',
   maxInstances: 2
 }).region(regionId).auth.user().onCreate(require('./user-on-create').handler);
