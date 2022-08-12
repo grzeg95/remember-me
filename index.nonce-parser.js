8a1a3994296e39531e9f627a9c4178111242e250
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2022-08-12 20:25:34 +0200

    feat: add crossorigin for NonAnonymousUrls

--- index.nonce-parser.js
+++ index.nonce-parser.js
@@ -4,7 +4,33 @@ const path = require('path');
 
 const indexPath = path.join(__dirname, 'public', 'html', 'index.html');
 const $ = cheerio.load(fs.readFileSync(indexPath));
-$('script').each((i, elem) => $(elem).attr('nonce', 'random-csp-nonce'));
+const script = $('script');
+const link = $('link');
+
+// nonce
+script.each((i, elem) => $(elem).attr('nonce', 'random-csp-nonce'));
+
+// crossorigin
+
+const crossOriginNonAnonymousUrls = new Set([
+  'https://consent.cookiebot.com/uc.js'
+]);
+
+const addOriginAnonymous = (elem) => {
+
+  const href = $(elem).attr('href');
+  const src = $(elem).attr('src');
+
+  if (crossOriginNonAnonymousUrls.has(href) || crossOriginNonAnonymousUrls.has(src)) {
+    $(elem).removeAttr('crossorigin');
+    return;
+  }
+
+  $(elem).attr('crossorigin', 'anonymous');
+}
+
+script.each((i, elem) => addOriginAnonymous(elem));
+link.each((i, elem) => addOriginAnonymous(elem));
 
 const index = $.html();
 const formattedIndex= index.replace(/(^\s*)|\n/gm, '');
