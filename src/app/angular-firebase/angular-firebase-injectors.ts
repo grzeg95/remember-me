78cf962cc798436034ea59a1576ed85eb4d56a96
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2022-09-13 09:01:08 +0200

    fix: remove unused APP_CHECK InjectionToken

--- src/app/angular-firebase/angular-firebase-injectors.ts
+++ src/app/angular-firebase/angular-firebase-injectors.ts
@@ -1,7 +1,6 @@
 import {InjectionToken} from '@angular/core';
 import {Analytics} from 'firebase/analytics';
 import {FirebaseApp} from 'firebase/app';
-import {AppCheck} from 'firebase/app-check';
 import {Auth} from 'firebase/auth';
 import {Firestore} from 'firebase/firestore';
 import {Functions} from 'firebase/functions';
@@ -9,7 +8,6 @@ import {RemoteConfig} from 'firebase/remote-config';
 
 export const ANALYTICS = new InjectionToken<Analytics>('Analytics');
 export const FIREBASE_APP = new InjectionToken<FirebaseApp>('FirebaseApp');
-export const APP_CHECK = new InjectionToken<AppCheck>('AppCheck');
 export const AUTH = new InjectionToken<Auth>('Auth');
 export const FIRESTORE = new InjectionToken<Firestore>('Firestore');
 export const FUNCTIONS = new InjectionToken<Functions>('Functions');
