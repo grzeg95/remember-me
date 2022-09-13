e82f350c93c087bb6607d21006790520b07bafed
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2022-09-13 08:58:45 +0200

    fix: source.subscribe
    deprecated Instead of passing separate callback arguments, use an observer argument. Signatures taking separate callback arguments will be removed in v8.

--- src/app/angular-firebase/tools.ts
+++ src/app/angular-firebase/tools.ts
@@ -4,10 +4,11 @@ import {Observable, OperatorFunction} from 'rxjs';
 export const runInZone = <T>(ngZone: NgZone): OperatorFunction<T, T> => {
   return (source) => {
     return new Observable<T>((observer) => {
-      const onNext = (value?: T) => ngZone.run(() => observer.next(value));
-      const onError = (e: any) => ngZone.run(() => observer.error(e));
-      const onComplete = () => ngZone.run(() => observer.complete());
-      return source.subscribe(onNext, onError, onComplete);
+      return source.subscribe({
+        next: (value?: T) => ngZone.run(() => observer.next(value)),
+        error: (e: any) => ngZone.run(() => observer.error(e)),
+        complete: () => ngZone.run(() => observer.complete())
+      });
     });
   };
 }
