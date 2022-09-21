7646e76a43bda56a3dc083eff70792db49e91c24
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2022-09-21 18:07:10 +0200

    fix: make app-loading-text leaner

--- src/app/loading-text/loading-text.component.ts
+++ src/app/loading-text/loading-text.component.ts
@@ -1,37 +1,40 @@
-import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
-import {BehaviorSubject, interval, Subscription} from 'rxjs';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-loading-text',
-  templateUrl: './loading-text.component.html',
-  styleUrls: ['./loading-text.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  template: '<span [innerHTML]="loadingText$ | async"></span>'
 })
-export class LoadingTextComponent implements OnInit, OnDestroy {
+export class LoadingTextComponent {
 
-  dots$: BehaviorSubject<string> = new BehaviorSubject<string>('...');
-  cnt = 0;
-  subscriptions: Subscription[] = [];
+  loadingText$ = new Observable<string>((subscriber) => {
 
-  ngOnInit(): void {
-    this.subscriptions.push(interval(333).subscribe(() => {
-      switch (this.cnt) {
-        case 0: this.dots$.next('...'); break;
-        case 1: this.dots$.next('<span class="invisible-dots">.</span>..'); break;
-        case 2: this.dots$.next('<span class="invisible-dots">..</span>.'); break;
-        case 3: this.dots$.next('<span class="invisible-dots">...</span>'); break;
-        case 4: this.dots$.next('.<span class="invisible-dots">..</span>'); break;
-        case 5: this.dots$.next('..<span class="invisible-dots">.</span>'); break;
+    const getLoadingText = (cnt: number) => {
+
+      let text = '';
+
+      switch (cnt) {
+        case 0: text = '<span class="invisible-text"></span>...'; break;
+        case 1: text = '<span class="invisible-text">.</span>..'; break;
+        case 2: text = '<span class="invisible-text">..</span>.'; break;
+        case 3: text = '<span class="invisible-text">...</span>'; break;
+        case 4: text = '.<span class="invisible-text">..</span>'; break;
+        case 5: text = '..<span class="invisible-text">.</span>'; break;
       }
-      if (this.cnt === 5) {
-        this.cnt = 0;
-      } else {
-        this.cnt++;
-      }
-    }));
-  }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
-  }
+      return 'Loading ' + text;
+    };
+
+    let cnt = 0;
+    subscriber.next(getLoadingText(cnt));
+
+    const intervalId = setInterval(() => {
+      subscriber.next(getLoadingText(cnt));
+      cnt = cnt === 5 ? 0 : cnt + 1;
+    }, 333);
+
+    return function unsubscribe() {
+      clearInterval(intervalId);
+    };
+  });
 }
