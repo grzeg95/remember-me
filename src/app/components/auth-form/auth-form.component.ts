49813e0dade01fb55c3f7bc38174757919ff22ac
Grzegorz Eleryk <dev.grzegorz.eleryk@gmail.com>
2024-08-24 17:50:29 +0200

    feat: fix auth nav logo and add loading animation the same as in normal nav

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -4,6 +4,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDialogRef} from '@angular/material/dialog';
 import {MatTabGroup} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {AuthService} from '../../services/auth.service';
 import {LoginComponent} from '../login/login.component';
 import {RegisterComponent} from '../register/register.component';
 import {SendPasswordResetEmailComponent} from '../send-password-reset-email/send-password-reset-email.component';
@@ -16,6 +17,10 @@ import {SendPasswordResetEmailComponent} from '../send-password-reset-email/send
   styleUrl: './auth-form.component.scss'
 })
 export class AuthFormComponent {
+
+  protected readonly _user = this._authService.userSig.get();
+  protected readonly _loadingUser = this._authService.loadingUserSig.get();
+  protected readonly _whileLoginIn = this._authService.whileLoginInSig.get();
 
   protected readonly _forms = [
     {
@@ -37,7 +42,8 @@ export class AuthFormComponent {
   @ViewChild('matTabGroup') matTabGroup: MatTabGroup | undefined;
 
   constructor(
-    protected readonly _dialogRef: MatDialogRef<AuthFormComponent>
+    protected readonly _dialogRef: MatDialogRef<AuthFormComponent>,
+    private readonly _authService: AuthService
   ) {
   }
 }
