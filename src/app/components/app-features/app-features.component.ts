f687ea1146722085c6e49e58397b20a8f6927c3b
Grzegorz Eleryk <dev.grzegorz.eleryk@gmail.com>
2024-09-18 16:04:02 +0200

    chore

--- src/app/components/app-features/app-features.component.ts
+++ src/app/components/app-features/app-features.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {SvgDirective} from '../../directives/svg.directive';
+import {ThemeSelectorService} from '../../services/theme-selector.service';
 
 @Component({
   selector: 'app-app-features',
   standalone: true,
-  imports: [],
+  imports: [
+    SvgDirective
+  ],
   templateUrl: './app-features.component.html',
-  styleUrl: './app-features.component.scss'
+  styleUrl: './app-features.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'app-features'
+  }
 })
 export class AppFeaturesComponent {
 
+  protected readonly _darkMode = this._themeSelectorService.darkModeSig.get();
+
+  constructor(
+    private readonly _themeSelectorService: ThemeSelectorService
+  ) {
+  }
 }
