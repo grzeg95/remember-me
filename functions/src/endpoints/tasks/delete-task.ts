806bbf91ee7a5d1e6199e6d2725e6487add51c41
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-11-29 17:58:13 +0100

    fix: handle timesOfDay removing in delete-task

--- functions/src/endpoints/tasks/delete-task.ts
+++ functions/src/endpoints/tasks/delete-task.ts
@@ -43,15 +43,37 @@ export const handler = async (request: CallableRequest) => {
     const round = roundSnap.data()!;
 
     const newTasksIds = round.tasksIds.filter((taskId) => taskId !== data.task.id);
-    transactionWrite.set(roundSnap.ref, {
-      ...round,
-      tasksIds: newTasksIds
-    });
 
     const taskRef = getTaskRef(roundSnap.ref, data.task.id);
     const taskSnap = await transaction.get(taskRef);
-    testRequirement(!roundSnap.exists, {code: 'invalid-argument'});
+    testRequirement(!taskSnap.exists, {code: 'invalid-argument'});
     const task = taskSnap.data()!;
+
+    transactionWrite.delete(taskSnap.ref);
+
+    const newTimesOfDay = [...round.timesOfDay];
+    const newTimesOfDayCardinality = [...round.timesOfDayCardinality];
+
+    const timesOfDaysToRemove = task.timesOfDay;
+
+    for (const timeOfDayToRemove of timesOfDaysToRemove) {
+
+      const index = newTimesOfDay.indexOf(timeOfDayToRemove);
+
+      if (newTimesOfDayCardinality[index] === 1) {
+        newTimesOfDay.splice(index, 1);
+        newTimesOfDayCardinality.splice(index, 1);
+      } else {
+        newTimesOfDayCardinality[index]--;
+      }
+    }
+
+    transactionWrite.set(roundSnap.ref, {
+      ...round,
+      tasksIds: newTasksIds,
+      timesOfDay: newTimesOfDay,
+      timesOfDayCardinality: newTimesOfDayCardinality
+    });
 
     for (const day of task.days) {
 
